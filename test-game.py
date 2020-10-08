30c85f710c56c427bf1d3a5a477075bedc308ba1
Knights Who Say Ni <62893792+KnightsWhoSayNi0@users.noreply.github.com>
2020-10-08 16:04:36 -0400

    Full Ring Support
    Added support for 2 character read which allows for all rings to be controlled. Added simple score as well.

--- test-game.py
+++ test-game.py
@@ -54,60 +54,66 @@ pygame.draw.circle(gameWindow, RED, (RedX[1], RedY[1]), 20, 4)
 
 pygame.display.update()
 
+# score setup
+#TODO: add game star button and subsequent code.
+score = 0
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+text = font.render(f'Score: {score}', True, BLACK, WHITE)
+textRect = text.get_rect()
+textRect.center = (800 // 2, 100 // 2)
+
 # main pygame loop
 while True:
+    
+    # draw score text
+    text = font.render(f'Score: {score}', True, BLACK, WHITE)
+    gameWindow.blit(text, textRect)
+    pygame.display.update()
+    
+    tdata1 =s1.read()
+    cdata1 =tdata1.decode("utf-8")
+    num1 = cdata1
+    tdata2 =s1.read()
+    cdata2 =tdata2.decode("utf-8")
+    num2 = cdata2
+    fullnum = int(num1 + num2)
+        
+    tdata = s1.read()
+    cdata = tdata.decode("utf-8")
+    if cdata == "o":
+        if fullnum < 7:
+            pygame.draw.circle(gameWindow, YELLOW, (YellowX[fullnum], YellowY[fullnum]), 20)
+            pygame.display.update()
+            pygame.mixer.Sound.play(lowBeep)
+            score = score + 1
+        if fullnum > 6 and fullnum < 14:
+            pygame.draw.circle(gameWindow, BLUE, (BlueX[fullnum-7], BlueY[fullnum-7]), 20)
+            pygame.display.update()
+            pygame.mixer.Sound.play(mediumBeep)
+            score = score + 2
+        if fullnum > 13 and fullnum < 16:
+            pygame.draw.circle(gameWindow, RED, (RedX[fullnum-14], RedY[fullnum-14]), 20)
+            pygame.display.update()
+            pygame.mixer.Sound.play(highBeep)
+            score = score + 3
+    else:
+        if fullnum < 7:
+            pygame.draw.circle(gameWindow, WHITE, (YellowX[fullnum], YellowY[fullnum]), 16)
+            pygame.display.update()
+        if fullnum > 6 and fullnum < 14:
+            pygame.draw.circle(gameWindow, WHITE, (BlueX[fullnum-7], BlueY[fullnum-7]), 16)
+            pygame.display.update()
+        if fullnum > 13 and fullnum < 16:
+            pygame.draw.circle(gameWindow, WHITE, (RedX[fullnum-14], RedY[fullnum-14]), 16)
+            pygame.display.update()
+        
     for event in pygame.event.get(): #TODO: fix broken quit
         if event.type == QUIT:
             pygame.quit()
+            quit()
             sys.exit()
-    
-    tdata =s1.read()
-    cdata =tdata.decode("utf-8")
-        
-    # if serial read 1 (yellow hole); blink yellow hole and beep
-    if cdata == "0":
-        tdata =s1.read()
-        cdata =tdata.decode("utf-8")
-        if cdata == "o":
-            pygame.draw.circle(gameWindow, YELLOW, (400, 550), 20)
-            pygame.display.update()
-            pygame.mixer.Sound.play(lowBeep)
-        else:
-            pygame.draw.circle(gameWindow, WHITE, (400, 550), 16)
-            pygame.display.update()
-        
-        #position = position + 50
-        #pygame.draw.rect(gameWindow, YELLOW, (50, 50, position, 50))
-    
-    # if serial read 2 (blue hole); blink blue hole and beep
-    if cdata == "1":
-        tdata =s1.read()
-        cdata =tdata.decode("utf-8")
-        if cdata == "o":
-            pygame.draw.circle(gameWindow, BLUE, (400, 500), 20)
-            pygame.display.update()
-            pygame.mixer.Sound.play(mediumBeep)
-        else:
-            pygame.draw.circle(gameWindow, WHITE, (400, 500), 16)
-            pygame.display.update()
-        
-        #position = position + 100
-        #pygame.draw.rect(gameWindow, BLUE, (50, 50, position, 50))
-        
-    # if serial read 3 (red hole); blink 
-    if cdata == "2":
-        tdata =s1.read()
-        cdata =tdata.decode("utf-8")
-        if cdata == "o":
-            pygame.draw.circle(gameWindow, RED, (400, 450), 20)
-            pygame.display.update()
-            pygame.mixer.Sound.play(highBeep)
-        else:
-            pygame.draw.circle(gameWindow, WHITE, (400, 450), 16)
-            pygame.display.update()
-        
-        #position = position + 200
-        #pygame.draw.rect(gameWindow, RED, (50, 50, position, 50))
+
 
 sys.exit()
             
